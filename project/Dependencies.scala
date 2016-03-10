6d2f935bf8e63cf465df2a17082eb8a47881d3a6
Christopher Hunt <huntchr@gmail.com>
2016-03-10 11:53:24 +1100

    Updated Lagom dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,7 @@ import sbt._
 import sbt.Resolver.bintrayRepo
 
 object Version {
-  // TODO: Use non SNAPSHOT version once Lagom published
-  val lagom             = "0.1.0-SNAPSHOT"
+  val lagom             = "1.0.0-M1"
   val sbtBundle         = "1.3.1"
   val play              = "2.3.10" // Using Play 2.3 to ensure compatibility with sbt-conductr
   val scala             = "2.10.6"
